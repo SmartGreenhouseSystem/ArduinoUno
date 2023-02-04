e1a571cf8adc63f5d9e416150c3acf308f040a06
NikolaLugumerski <nikolalugumerski123@gmail.com>
2023-02-04 14:49:39 +0100

    Added reading from all sensors

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 #include <DHT.h>
 #include <SoftwareSerial.h>
-#define DHTPIN 0
-#define DHTPIN 1
-#define DHTPIN 2
+#define DHTPING 2
+#define DHTPINA 3
+#define DHTPINP 4
 
  
 // Uncomment whatever type you're using!
@@ -14,9 +14,9 @@
 #define TX 6
 
 SoftwareSerial espSerial(RX, TX);
-DHT dhtGreenhouse(DHTPIN, DHTTYPE);
-DHT dhtAtmospheric(DHTPIN, DHTTYPE);
-DHT dhtPile(DHTPIN, DHTTYPE);
+DHT dhtGreenhouse(DHTPING, DHTTYPE);
+DHT dhtAtmospheric(DHTPINA, DHTTYPE);
+DHT dhtPile(DHTPINP, DHTTYPE);
 
 String str;
  
@@ -54,7 +54,7 @@ void loop()
   else 
   {
     str = String("GT|" + String(tG));
-    allData = allData + '$' + str;
+    allData = allData + '$' + str + '$';
   }
 
   float hA = dhtAtmospheric.readHumidity();
@@ -68,13 +68,32 @@ void loop()
   }
   
   float tA = dhtAtmospheric.readTemperature();
-
   if (isnan(tA)) {
       return;
   }
   else 
   {
     str = String("AT|" + String(tA));
+    allData = allData + '$' + str + '$';
+  }
+
+  float hP = dhtPile.readHumidity();
+  if (isnan(hP)) {
+      return;
+  }
+  else 
+  {
+    str =String("PH|" + String(hP));
+    allData = allData + str;
+  }
+  
+  float tP = dhtPile.readTemperature();
+  if (isnan(tP)) {
+      return;
+  }
+  else 
+  {
+    str = String("PT|" + String(tP));
     allData = allData + '$' + str;
   }
 
