0249a570a578d37a402d6fec6eda958df0e42345
NikolaLugumerski <nikolalugumerski123@gmail.com>
2023-08-19 14:10:58 +0200

    New working order with sensors

--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,165 @@
 #include <Arduino.h>
 #include <DHT.h>
 #include <SoftwareSerial.h>
-#define DHTPIN 0
-#define DHTPIN 1
-#define DHTPIN 2
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
- 
-// Uncomment whatever type you're using!
-#define DHTTYPE DHT11 // DHT 11
-//#define DHTTYPE DHT22 // DHT 22 (AM2302), AM2321
-//#define DHTTYPE DHT21 // DHT 21 (AM2301)
+
+#define DHTPIN1 2
+#define DHTPIN2 3
+
+#define SOIL_MOISTURE_PIN1 A0
+#define SOIL_MOISTURE_PIN2 A1
+
+#define ONE_WIRE_BUS 4
+#define DHTTYPE DHT22 
+
 #define RX 5
 #define TX 6
+#define SENSOR_COUNT 8 
+#define SENSOR_WAIT_TIME 900000 / SENSOR_COUNT
 
-SoftwareSerial espSerial(RX, TX);
-DHT dhtGreenhouse(DHTPIN, DHTTYPE);
-DHT dhtAtmospheric(DHTPIN, DHTTYPE);
-DHT dhtPile(DHTPIN, DHTTYPE);
+/*SoftwareSerial espSerial(RX, TX);*/
+OneWire oneWire(ONE_WIRE_BUS);
+
+
+DHT dhtPlant1(DHTPIN1, DHTTYPE);
+DHT dhtPlant2(DHTPIN2, DHTTYPE);
 
 String str;
- 
+String sensorData[SENSOR_COUNT];
+DallasTemperature soilTempSensors(&oneWire);
+
+int numberOfSoilMoistureSensors;
+DeviceAddress tempSMSAddress;
+
+const int drySoilMoisture = 595; // value for dry sensor
+const int wetSoilMoisture = 239; // value for wet sensor
+
+
 void setup(){
 pinMode(RX, INPUT);
 pinMode(TX, OUTPUT);
 
 Serial.begin(115200);
-espSerial.begin(9600);
-dhtGreenhouse.begin();
-dhtAtmospheric.begin();
-dhtPile.begin();
+/*espSerial.begin(9600);*/
+dhtPlant1.begin();
+dhtPlant2.begin();
+soilTempSensors.begin();
+
+
+numberOfSoilMoistureSensors = soilTempSensors.getDeviceCount();
+Serial.println(numberOfSoilMoistureSensors, DEC);
+
+
 delay(2000);
 }
  
  
 void loop()
 {
-  String allData = "";
   
-  float hG = dhtGreenhouse.readHumidity();
-  if (isnan(hG)) {
-      return;
-  }
-  else 
-  {
-    str =String("GH|" + String(hG));
-    allData = allData + str;
-  }
+  float hP1 = dhtPlant1.readHumidity();
   
-  float tG = dhtGreenhouse.readTemperature();
-  if (isnan(tG)) {
-      return;
+  if (isnan(hP1)) {
+     return;
   }
   else 
   {
-    str = String("GT|" + String(tG));
-    allData = allData + '$' + str;
-  }
-
-  float hA = dhtAtmospheric.readHumidity();
-  if (isnan(hA)) {
-      return;
-  }
-  else 
-  {
-    str =String("AH|" + String(hA));
-    allData = allData + str;
+    str =String("hP1|" + String(hP1));
+    sensorData[0] = str;
   }
   
-  float tA = dhtAtmospheric.readTemperature();
-
-  if (isnan(tA)) {
+  float tP1 = dhtPlant1.readTemperature();
+  if (isnan(tP1)) {
       return;
   }
   else 
   {
-    str = String("AT|" + String(tA));
-    allData = allData + '$' + str;
+    str = String("tP1|" + String(tP1));
+    sensorData[1] = str;
   }
 
-  Serial.println(allData);
-  espSerial.println(allData);
+  float hP2 = dhtPlant2.readHumidity();
+  if (isnan(hP2)) {
+      return;
+  }
+  else 
+  {
+    str =String("hP2|" + String(hP2));
+    sensorData[2] = str;
+  }
+  
+  float tP2 = dhtPlant2.readTemperature();
 
+  if (isnan(tP2)) {
+      return;
+  }
+  else 
+  {
+    str = String("tP2|" + String(tP2));
+    sensorData[3] = str;
+  }
+
+  int smP1 = analogRead(SOIL_MOISTURE_PIN1);
+
+  if (isnan(smP1)) {
+      return;
+  }
+  else 
+  {
+    int soilMoistureP1 = map(smP1, wetSoilMoisture, drySoilMoisture, 100, 0); 
+    str = String("smP1|" + String(soilMoistureP1));
+    sensorData[4] = str;
+  }
+
+  int smP2 = analogRead(SOIL_MOISTURE_PIN2);
+
+  if (isnan(smP2)) {
+      return;
+  }
+  else 
+  {
+    int soilMoistureP2 = map(smP2, wetSoilMoisture, drySoilMoisture, 100, 0); 
+    str = String("smP2|" + String(soilMoistureP2));
+    sensorData[5] = str;
+  }
+
+  soilTempSensors.requestTemperatures();
+
+  int stP1 = soilTempSensors.getTempCByIndex(0);
+  if (isnan(stP1)) {
+    return;
+  }
+  else
+  {
+    str =String("stP1|" + String(stP1));
+    sensorData[6] = str;
+  }
+  
+  int stP2 = soilTempSensors.getTempCByIndex(1);
+  if (isnan(stP2)) {
+    return;
+  }
+  else
+  {
+    str =String("stP2|" + String(stP2));
+    sensorData[7] = str;
+  }
+    
+
+  Serial.println("Acquired all sensor data ---> ");
+  
   delay(1000);
+
+  for(int i = 0; i < SENSOR_COUNT; i++) 
+  {
+    /*espSerial.println(sensorData[i]);*/
+    Serial.println("Sensor:" + sensorData[i]);
+    delay(3000);
+  }
+  
+  Serial.println("Done sending sensor data");
+  delay(1000);
+  
 }
